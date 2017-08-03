75af06c5730a8c8ff5a64e250f7667a6e8658e14
Ian Vilhena <Ian@Ians-MacBook-Pro.local>
2017-08-03 19:54:17 -0300

    Initial Commit

---
+++ JSON/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSON
+//
+//  Created by Ian Vilhena on 8/3/17.
+//  Copyright © 2017 Ian Vilhena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
